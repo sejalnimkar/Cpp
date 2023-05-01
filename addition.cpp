1b6bcb70e5ce7669f822aabc4da9e55afe894729
sejalnimkar <83336554+sejalnimkar@users.noreply.github.com>
2023-05-01 11:06:54 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter first no."
+    cin>>n1;
+    cout<<"Enter second no.";
+    cin>>n2;
+    cout<<"Addition is "<<n1+n1;
+    return 0;
+}
